8b9881a72a0c5f1ae36076d381766a21af2dfaad
Belinschi Dima <belinski.d@gmail.com>
2016-03-21 02:39:35 +0200

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
 
-  resources :answers
+
   resources :qbuilders
-  resources :polls
-  resources :questions
+  resources :polls do
+    resources :questions do
+      resources :answers
+    end
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
