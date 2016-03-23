36cc9a77d7193ecce5e6e3f3385044ecc5dbfbee
Belinschi Dima <belinski.d@gmail.com>
2016-03-23 22:12:30 +0200

    Fix redirect after update question

--- app/controllers/formquestions_controller.rb
+++ app/controllers/formquestions_controller.rb
@@ -44,7 +44,7 @@ class FormquestionsController < ApplicationController
     #binding.pry
     respond_to do |format|
       if @formquestion.update(formquestion_params)
-        format.html { redirect_to :back, notice: 'Question was successfully updated.' }
+        format.html { redirect_to formquestion_path, notice: 'Question was successfully updated.' }
         format.json { render :show, status: :ok, location: @formquestion }
       else
         format.html { render :edit }
