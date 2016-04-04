59643c95fe121e8d14fed8c588dd871e70e7a18e
Belinschi Dima <belinski.d@gmail.com>
2016-04-04 16:26:09 +0300

    delete models newcategory newquestion

--- app/models/newcategory.rb
+++
@@ -1,5 +0,0 @@
-class Newcategory < ApplicationRecord
-  validates :new_category_name, presence: true
-  validates :new_category_score, presence: true, numericality: { only_integer: true }
-  has_many :newquestions, :dependent => :destroy
-end
