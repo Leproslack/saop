f87fb528186194030718b62ac5801cae2039366f
Belinschi Dima <belinski.d@gmail.com>
2016-04-13 00:05:56 +0300

    migrate to pg from sqlite

--- db/migrate/20160321202838_delete_columns_from_poll.rb
+++
@@ -1,8 +0,0 @@
-class DeleteColumnsFromPoll < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :polls, :pos_name
-    remove_column :polls, :pos_rate
-    remove_column :polls, :stuff_rate
-    remove_column :polls, :stuff_name
-  end
-end
