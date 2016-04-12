17c95bbccaca30c25fac2e3dd398e77c6bdce412
Belinschi Dima <belinski.d@gmail.com>
2016-04-12 21:27:36 +0300

    test to calculate score result for Poll

---
+++ spec/factories/poll.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :poll do
+    name "Poll test"
+  end
+end
---
+++ spec/factories/question.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :question do
+    content "Question test"
+    score_result 1
+  end
+end
