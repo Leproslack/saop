d993ee16ad747493150ca46ffda34e0de3f57963
Belinschi Dima <belinski.d@gmail.com>
2016-04-12 20:13:21 +0300

    refactoring calculate score Template

--- app/models/template_field.rb
+++ app/models/template_field.rb
@@ -2,14 +2,5 @@ class TemplateField < ApplicationRecord
   validates :question, presence: true
   validates :score, presence: true, length: { in: 1..2 }
   belongs_to :template, optional: true
-
-  after_commit    :calculate_score_init
-
-  def calculate_score_init
-    if Template.exists?(template_id)
-      score_init = Template.find(template_id).template_fields.sum(:score)
-      Template.find(template_id).update!(score: score_init)
-    end
-  end
-
+  
 end
