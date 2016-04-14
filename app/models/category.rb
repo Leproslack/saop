e5910307de95dedba50833cdf9eadd9205b822e0
Belinschi Dima <belinski.d@gmail.com>
2016-04-14 18:40:50 +0300

    refactoring#

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,13 +3,4 @@ class Category < ApplicationRecord
   belongs_to :poll, optional: true
   has_many :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions
-
-  # after_commit    :calculate_category_score_result
-  #
-  # def calculate_category_score_result
-  #   if Poll.exists?(poll_id)
-  #     score_result = Poll.find(poll_id).categories.sum(:score_result)
-  #     Poll.find(poll_id).update!(score_result: score_result)
-  #   end
-  # end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,13 +2,4 @@ class Question < ApplicationRecord
   validates :score_result, presence: true, numericality: { only_integer: true }
   validates :content, presence: true
   belongs_to :category, optional: true
-
-  # after_commit    :calculate_category_score_result
-  #
-  # def calculate_category_score_result
-  #   if Category.exists?(category_id)
-  #     score_result = Category.find(category_id).questions.sum(:score_result)
-  #     Category.find(category_id).update!(score_result: score_result)
-  #   end
-  # end
 end
